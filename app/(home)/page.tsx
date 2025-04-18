dbb32927f516b37d8ee721e91cf6291980d0b084
liuliangzheng <liuliangzheng520@163.com>
2025-04-18 15:45:03 +0800

    asdf

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">欢迎使用taoke-mcp</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        点击这里开始{' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        来查看使用文档
       </p>
     </main>
   );
