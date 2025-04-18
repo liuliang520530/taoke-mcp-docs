2127e96adc31ea0d373b7634c7a71c3b1ef1df61
liuliangzheng <liuliangzheng520@163.com>
2025-04-18 22:06:17 +0800

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [new URL('http://bbs.weiququ.cn/template/weiququ/static/imgs/qrgroup.png')],
+  },
 };
 
 export default withMDX(config);
