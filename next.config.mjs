cb27d409b1dea0c3cc6d69324e27dd4cd58e4ea6
liuliangzheng <liuliangzheng520@163.com>
2025-04-18 15:23:53 +0800

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+
+  // 使用静态导出模式
+  output: 'export',
+
+  // 禁用图像优化，适用于静态导出
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withMDX(config);
+
